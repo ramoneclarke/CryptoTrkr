5a784b40255fb13d87e87a170b0c74216b30ee75
Ramone Clarke <ramonejlclarke@gmail.com>
2022-04-29 16:22:32 +0100

    feat: create useFetch hook to reduce boilerplate

---
+++ crypto-tracker/src/utils/useFetch.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    setData(null);
+    setError(null);
+    const controller = new AbortController();
+    axios
+      .get(url, { signal: controller.signal })
+      .then((res) => {
+        setLoading(false);
+        res.data && setData(res.data);
+      })
+      .catch((err) => {
+        setLoading(false);
+        setError(err);
+      });
+
+    return () => {
+      controller.abort();
+    };
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
