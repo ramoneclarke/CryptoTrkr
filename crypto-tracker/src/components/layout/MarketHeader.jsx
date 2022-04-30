eb357467870261b14d226d41fc816ea5a621e0c8
Ramone Clarke <ramonejlclarke@gmail.com>
2022-04-30 15:50:49 +0100

    refactor: delete MarketTableMobile as it's now unused due to media queries

--- crypto-tracker/src/components/layout/MarketHeader.jsx
+++ crypto-tracker/src/components/layout/MarketHeader.jsx
@@ -5,25 +5,6 @@ import FilterSearchBar from "../FilterSearchBar";
 
 const MarketHeader = () => {
   return (
-    // <Grid container mt={5} mb={2}>
-    //   {/* <Grid
-    //     item
-    //     xs={12}
-    //     sx={{
-    //       display: "flex",
-    //       justifyContent: "center",
-    //       alignItems: "center",
-    //     }}
-    //   >
-    //     <Typography variant="h3">Market</Typography>
-    //   </Grid> */}
-    //   <Grid item xs={6}>
-    //     <FilterSearchBar placeholder="Filter coins..." />
-    //   </Grid>
-    //   <Grid item xs={6}>
-    //     <CurrencySelector currentCurrency={"gbp"} />
-    //   </Grid>
-    // </Grid>
     <Box
       sx={{
         mt: {
--- crypto-tracker/src/pages/Market.jsx
+++ crypto-tracker/src/pages/Market.jsx
@@ -1,18 +1,10 @@
-import {
-  Box,
-  Grid,
-  Stack,
-  Typography,
-  useMediaQuery,
-  useTheme,
-} from "@mui/material";
+import { Box, useTheme } from "@mui/material";
 import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 import { useContext } from "react";
 import MarketHeader from "../components/layout/MarketHeader";
 import MarketTable from "../components/layout/MarketTable";
-import MarketTableMobile from "../components/layout/MarketTableMobile";
 import { AppContext } from "../context/AppContext";
 import { DataContext } from "../context/DataContext";
 
@@ -59,32 +51,6 @@ const Market = () => {
       }))
     );
   }, [coinData, currency]);
-
-  // const isSmallDevice = useMediaQuery(theme.breakpoints.down("md"));
-
-  // if (isSmallDevice) {
-  //   return (
-  //     <Box sx={{ width: "100%", height: "100%" }}>
-  //       <MarketHeader />
-  //       <MarketTableMobile marketData={marketData} />
-  //     </Box>
-  //   );
-  // } else {
-  //   return (
-  //     <Box
-  //       sx={{
-  //         display: "flex",
-  //         flexDirection: "column",
-  //         justifyContent: "flex-start",
-  //         alignItems: "flex-start",
-  //         width: "100%",
-  //       }}
-  //     >
-  //       <MarketHeader />
-  //       <MarketTable marketData={marketData} />
-  //     </Box>
-  //   );
-  // }
 
   return (
     <Box
