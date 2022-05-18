27985977388d9106c3f954fcb1c984325f3f3674
Ramone Clarke <ramonejlclarke@gmail.com>
2022-05-18 20:34:10 +0100

    feat: fill out watchlist pop up box with coins

--- crypto-tracker/src/components/AddToWatchListPopUpBox.jsx
+++ crypto-tracker/src/components/AddToWatchListPopUpBox.jsx
@@ -1,9 +1,29 @@
-import { Box, Dialog, DialogTitle, Divider, List } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  Divider,
+  Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Stack,
+  Typography,
+} from "@mui/material";
 import React from "react";
+import { useContext } from "react";
+import { useEffect } from "react";
 import { useState } from "react";
+import { DataContext } from "../context/DataContext";
 import FilterSearchBar from "./FilterSearchBar";
 
 const AddToWatchListPopUpBox = ({ onClose, selectedValue, open }) => {
+  const useDataContext = useContext(DataContext);
+  const { coinData, isLoading } = useDataContext;
+
   const [filterText, setFilterText] = useState("");
 
   const handleClose = () => {
@@ -18,6 +38,19 @@ const AddToWatchListPopUpBox = ({ onClose, selectedValue, open }) => {
       onClose={handleClose}
       open={open}
       maxWidth="xl"
+      sx={{
+        "& ::-webkit-scrollbar": {
+          width: "0.4em",
+        },
+        "& ::-webkit-scrollbar-track": {
+          boxShadow: "inset 0 0 6px rgba(0,0,0,0.00)",
+          webkitBoxShadow: "inset 0 0 6px rgba(0,0,0,0.00)",
+        },
+        "& ::-webkit-scrollbar-thumb": {
+          backgroundColor: "rgba(0,0,0,.1)",
+          outline: "1px solid slategrey",
+        },
+      }}
       PaperProps={{
         sx: {
           height: "80vh",
@@ -44,7 +77,45 @@ const AddToWatchListPopUpBox = ({ onClose, selectedValue, open }) => {
         MB={{ xs: 1, md: 0 }}
         width="90%"
       />
-      {/* <List></List> */}
+      <List
+        sx={{
+          width: "100%",
+        }}
+      >
+        {coinData.map((coin, index) => (
+          <ListItem
+            disablePadding
+            key={index}
+            divider
+            sx={{ display: "flex", justifyContent: "flex-end" }}
+          >
+            <ListItemButton>
+              <Typography variant="subtitle1">{index + 1}</Typography>
+              <Avatar
+                alt={coin.name}
+                src={coin.image}
+                sx={{ m: "0 1rem 0 1rem" }}
+              />
+              <Stack direction="row" width="100%">
+                <Grid container spacing={1}>
+                  <Grid item xs={3}>
+                    <ListItemText
+                      primary={coin.symbol.toUpperCase()}
+                      primaryTypographyProps={{
+                        textAlign: "left",
+                        color: "text.secondary",
+                      }}
+                    />
+                  </Grid>
+                  <Grid item xs={9}>
+                    <ListItemText primary={coin.name} />
+                  </Grid>
+                </Grid>
+              </Stack>
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
     </Dialog>
   );
 };
