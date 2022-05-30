3e00e6feb9d30f913ad12bb190c4e7ea8d91d009
Ramone Clarke <ramonejlclarke@gmail.com>
2022-05-30 16:43:46 +0100

    create number formatters for percentages and varied sizes of numbers

--- crypto-tracker/src/utils/numberFormatters.js
+++ crypto-tracker/src/utils/numberFormatters.js
@@ -1,22 +1,49 @@
-export const largeNumberFormatter = new Intl.NumberFormat("en-IN", {
+export const largeNumberFormatter = new Intl.NumberFormat("en-US", {
   // Numbers >= 1
   minimumFractionDigits: 2,
   maximumFractionDigits: 2,
 });
 
-export const smallNumberFormatter = new Intl.NumberFormat("en-IN", {
+export const smallNumberFormatter = new Intl.NumberFormat("en-US", {
   // Numbers < 1 && > 0.01
   minimumFractionDigits: 4,
   maximumFractionDigits: 5,
   roundingIncrement: 5,
 });
 
-export const extraSmallNumberFormatter = new Intl.NumberFormat("en-IN", {
+export const extraSmallNumberFormatter = new Intl.NumberFormat("en-US", {
   // Numbers < 0.01
   minimumFractionDigits: 2,
-  maximumSignificantDigits: 12,
+  maximumFractionDigits: 12,
   roundingIncrement: 5,
 });
+
+export const largeCurrencyFormatter = (number) => {
+  return new Intl.NumberFormat("en-US", {
+    maximumSignificantDigits: 18,
+  }).format(number);
+};
+
+export const percentageFormatter = (number) => {
+  if (number >= 10) {
+    return new Intl.NumberFormat("en-US", {
+      minimumSignificantDigits: 4,
+      maximumSignificantDigits: 4,
+    }).format(number);
+  } else if (number > 1 && number < 10) {
+    return new Intl.NumberFormat("en-US", {
+      minimumSignificantDigits: 3,
+      maximumSignificantDigits: 3,
+      style: "decimal",
+      signDisplay: "never",
+    }).format(number);
+  } else {
+    return new Intl.NumberFormat("en-US", {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    }).format(number);
+  }
+};
 
 export const numberFormatter = (number) => {
   if (number >= 1) {
