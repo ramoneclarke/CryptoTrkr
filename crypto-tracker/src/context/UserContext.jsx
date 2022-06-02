2a6f2054ac022afe52dda519be241a5be35117a0
Ramone Clarke <ramonejlclarke@gmail.com>
2022-06-02 16:53:02 +0100

    create user portfolio context structure and reducer actions

--- crypto-tracker/src/context/UserContext.jsx
+++ crypto-tracker/src/context/UserContext.jsx
@@ -6,9 +6,11 @@ import { createContext } from "react";
 export const UserContext = createContext();
 
 const initialState = {
-  // watchList: ["bitcoin", "ethereum"],
-  watchList: [],
-  portfolio: {},
+  watchList: [], // an array of coin ids
+  portfolio: {}, // object containing portfolio item objects -> coinId: {coin id, amount held, transactions}
+  // transactions: [], //  an array of objects. One for each transaction.
+  // transaction object -> coin id, buy/sell, quantity, currency bought with, date
+  transactionHistory: [], // array of transaction objects, appended after each logged transaction
 };
 
 const reducer = (state, action) => {
@@ -25,6 +27,56 @@ const reducer = (state, action) => {
           ...state.watchList.filter((coinId) => coinId !== action.payload),
         ],
       };
+    case "addToPortfolio":
+      // takes a coin id
+      let addToPortfolioObj = state.portfolio;
+      addToPortfolioObj[action.payload] = {
+        coinId: action.payload,
+        holdings: 0,
+        transactions: [],
+      };
+
+      return {
+        ...state,
+        portfolio: { ...addToPortfolioObj },
+      };
+    case "removeFromPortfolio":
+      // takes a coin id
+      delete state.portfolio[action.payload];
+      return {
+        ...state,
+      };
+    case "addTransaction":
+      // takes an object containing transaction info
+
+      const transactionObj = {
+        coinId: action.payload.id,
+        type: action.payload.type,
+        quantity: action.payload.quantity,
+        currency: action.payload.currency,
+        date: action.payload.date,
+      };
+
+      let updatedHoldings = state.portfolio[action.payload.id].holdings;
+      action.payload.type === "buy"
+        ? (updatedHoldings += action.payload.quantity)
+        : (updatedHoldings += action.payload.quantity);
+
+      return {
+        ...state,
+        portfolio: {
+          ...state.portfolio,
+          [action.payload.id]: {
+            coinId: action.payload.id,
+            holdings: updatedHoldings,
+            transactions: [
+              ...state.portfolio[action.payload.id].transactions,
+              transactionObj,
+            ],
+          },
+        },
+        transactionHistory: [...state.transactionHistory, transactionObj],
+      };
     default:
       return state;
   }
@@ -32,10 +84,12 @@ const reducer = (state, action) => {
 
 export const UserContextProvider = ({ children }) => {
   const [state, dispatchUserContext] = useReducer(reducer, initialState);
-  const { watchList, portfolio } = state;
+  const { watchList, portfolio, transactionHistory } = state;
 
   return (
-    <UserContext.Provider value={{ watchList, portfolio, dispatchUserContext }}>
+    <UserContext.Provider
+      value={{ watchList, portfolio, transactionHistory, dispatchUserContext }}
+    >
       {children}
     </UserContext.Provider>
   );
